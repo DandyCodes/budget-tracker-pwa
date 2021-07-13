d882383d6ffeea06fa91ae48290e829ea1963c49
John Barker <jdabarker@gmail.com>
2021-07-13 17:39:06 +0800

    added cache key cleanup on activate handler for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,12 +11,26 @@ const staticPaths = [
 const staticCacheName = "static-cache-v1";
 const dynamicCacheName = "dynamic-cache-v1";
 
-self.addEventListener("install", async event => {
+self.addEventListener("install", async () => {
   try {
     const cache = await caches.open(staticCacheName);
     await cache.addAll(staticPaths);
+    self.skipWaiting();
   } catch (err) {
     console.log(err);
   }
-  self.skipWaiting();
+});
+
+self.addEventListener("activate", async () => {
+  try {
+    const keyList = await caches.keys();
+    for (const key of keyList) {
+      if (key !== staticCacheName && key !== dynamicCacheName) {
+        await caches.delete(key);
+      }
+    }
+    self.clients.claim();
+  } catch (err) {
+    console.log(err);
+  }
 });
